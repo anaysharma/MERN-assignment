28e2c7cc144b284af3d7b8be5b007d4fd86213f9
Anay Sharma <anaysharma9891@gmail.com>
2024-10-01 00:23:45 +0530

    UPDATE: vercel config in frontend

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
-import { server_url as URL } from './config.json';
 
 export const login = async (payload) => {
   try {
-    const { data } = await axios.post(URL + 'login', payload);
+    const { data } = await axios.post(
+      import.meta.env.VITE_BACKEND_URL + 'login',
+      payload
+    );
     const { t } = data;
     localStorage.setItem('token', t);
     return data;
@@ -14,7 +16,10 @@ export const login = async (payload) => {
 
 export const singup = async (payload) => {
   try {
-    const { data } = await axios.post(URL + 'signup', payload);
+    const { data } = await axios.post(
+      import.meta.env.VITE_BACKEND_URL + 'signup',
+      payload
+    );
     return data;
   } catch (err) {
     console.error(err);
@@ -24,12 +29,15 @@ export const singup = async (payload) => {
 export const getUserDetails = async () => {
   try {
     const token = localStorage.getItem('token');
-    const { data } = await axios.get(URL + 'who-am-i', {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: token,
-      },
-    });
+    const { data } = await axios.get(
+      import.meta.env.VITE_BACKEND_URL + 'who-am-i',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: token,
+        },
+      }
+    );
     return data;
   } catch (err) {
     console.error(err);
